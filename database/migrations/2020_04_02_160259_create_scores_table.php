be5d314662b2be7a226ad470fb2bdd826adbcb3f
Ali-Sharifi-Neyestani <hacka.kootlas@gmail.com>
2020-04-02 20:40:17 +0430

    add simple db for fast-select game

---
+++ database/migrations/2020_04_02_160259_create_scores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->id();
+            $table->string('alias');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->nullable();
+            $table->foreignId('game_id')->constrained()->onDelete('cascade')->nullable();
+            $table->integer('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
